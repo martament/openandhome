63ca952597d23389e5b09279815b17b927902933
martament <38855494+martament@users.noreply.github.com>
2019-01-11 16:28:04 +0100

    Delete set_config.sh

--- configs/espeasy/set_config.sh
+++
@@ -1,4 +0,0 @@
-configurations for the openandhome sensors.
-There are two kinds of sensors:
-- mqtt => these configs are for sensors included in a smarthome with mqtt enabled and configured for the openandhome-Server
-- standalone => these confis are for sensors without mqtt and smarthome
