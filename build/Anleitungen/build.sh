0e49b875c1d9e31f41126a59404c2cb1a4cb9f6a
Ament Martin <martin.ament@gmx.de>
2020-05-08 12:33:39 +0200

    new build and .gitignore for WLED

--- build/Anleitungen/build.sh
+++ build/Anleitungen/build.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 # Skript to build the docs from Word
-soffice --headless --convert-to pdf Openandhome_Bedienungsanleitung_Temperatursensor.pdf '../../../../14\ -\ Kunde\ Bedienungsanleitung/2020\ openandhome\ -\ Temperatur\ Bedienungsanleitung.docx'
+cur_dir=$PWD
+cd ../../../../14\ -\ Kunde\ Bedienungsanleitung
+pwd
+soffice --headless --convert-to pdf Openandhome_Bedienungsanleitung_Temperatursensor.pdf '2020 openandhome - Temperatur Bedienungsanleitung.docx'
 #Move to Target
-mv '2020 openandhome - Temperatur Bedienungsanleitung.pdf' ../../docs/'openandhome - Temperatur Bedienungsanleitung.pdf'
+mv '2020 openandhome - Temperatur Bedienungsanleitung.pdf' '../01 - Entwicklung/openandhome/docs/openandhome - Temperatur Bedienungsanleitung.pdf'
+pwd
+cd "$cur_dir"
