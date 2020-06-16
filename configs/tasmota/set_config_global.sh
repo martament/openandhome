6de863f7275af9670bea200e00da2f53bb048b1a
Ament Martin <martin.ament@gmx.de>
2020-06-16 18:51:43 +0200

    New Releases added

--- configs/tasmota/set_config_global.sh
+++ configs/tasmota/set_config_global.sh
@@ -28,20 +28,29 @@ case $l_name in
 esac
 #Setze den Modultypen
 wget -O/dev/null http://$1/cm?cmnd=Module%20$l_module
-sleep 4
+sleep 10
 echo
 echo "Setze MQTT-Parameter"
 wget -O/dev/null http://$1/cm?cmnd=MqttClient%20$2
-sleep 3
+sleep 8
+wget -O/dev/null http://$1/cm?cmnd=MqttUser%20openhabian
+sleep 8
+wget -O/dev/null http://$1/cm?cmnd=MqttPassword%20openandhome%40Raspi
+sleep 8
 echo "Setze Netzwerk-Parameter"
 wget -O/dev/null http://$1/cm?cmnd=Hostname%20Sonoff-$2
-sleep 1
+sleep 8
+wget -O/dev/null http://$1/cm?cmnd=DeviceName%20$2
+sleep 8
 wget -O/dev/null http://$1/cm?cmnd=FriendlyName%20$2
-sleep 1
+sleep 5
 #Client durchstarten um 20 nach 12
 wget -O/dev/null http://$1/cm?cmnd=rule1%20on%20Time%23Minute=345%20do%20restart%201%20endon
-sleep 1
+sleep 5
 wget -O/dev/null http://$1/cm?cmnd=rule1%20on
-sleep 3
+sleep 5
 wget -O/dev/null http://$1/cm?cmnd=FullTopic%20%25prefix%25%2F%25topic%25%2F$2%2F
+sleep 5
+#SetOption53 	Display hostname and IP address in GUI
+wget -O/dev/null http://$1/cm?cmnd=SetOption53%201
 echo "Fertig mit set_config_global.sh"
