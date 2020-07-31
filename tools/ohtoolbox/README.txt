5d3ea6aae0ba52b048c12beddbd2cd73c9d8fc3b
Ament Martin <martin.ament@gmx.de>
2020-07-31 16:25:31 +0200

    added full_reset.bat Added description

--- tools/ohtoolbox/README.txt
+++ tools/ohtoolbox/README.txt
@@ -1,4 +1,16 @@
+Versuchen Sie immer zuerst das Programm reset_openandhome.bat !!!!!
+
+Achten Sie bitte darauf, dass die Datei SerialSend.exe bei Windows im gleichen Verzeichnis ist.
+Ebenso muss der Sensor mit dem Rechner über USB verbunden sein.
+
+reset_openandhome.bat:
+
 Mit dem Programm reset_openandhome.bat (reset_openandhome.sh für Linux) können Sie die Einstellungen des Sensors zurück setzen. 
 Es werden die WIFI-Zugangsdaten und das Zugangspasswort auf den Auslieferungszustand gesetzt. 
 
-Achten Sie bitte darauf, dass die Datei SerialSend.exe bei Windows im gleichen Verzeichnis ist.
+full_reset.bat:
+Mit diesem Programm können Sie den Sensor vollständig zurück setzen. Es werden alle Einstellungen des Sensor gelöscht.
+Sie müssen den Sensor danach von Grund auf neu konfigurieren.
+Eine Anleitung dazu finden Sie in https://www.openandhome.de/index.php/ufaqs/waere-es-moeglich-mir-die-daten-fuer-die-grundfiguration-zukommen-zu-lassen/ 
+
+
---
+++ tools/ohtoolbox/full_reset.bat
@@ -0,0 +1,23 @@
+@echo "Achtung dieses Skript löscht alle Einstellungen des Sensors..." 
+@echo "Versuchen Sie immer zuerst das Skript reset_openandhome.bat" 
+@echo "" 
+@echo "Sie muessen den Sensor danach erneut von Grund auf konfigurieren." 
+@echo "" 
+@echo "Achten Sie drauf, dass sich die Datei SerialSend.exe im gleichen Verzeichnis befindet." 
+@echo "" 
+@echo "..." 
+@echo "Sie sollten genau wissen was Sie tun." 
+@echo "..." 
+@echo "..." 
+@echo "Druecken Sie eine beliebige Taste . . ." 
+pause
+SerialSend.exe /baudrate 115200 /closedelay 500 /hex "Reset\r\n" 
+@echo "Druecken Sie eine beliebige Taste . . ." 
+pause
+SerialSend.exe /baudrate 115200  /closedelay 500 /hex "reboot\r\n"
+@echo "" 
+@echo "Wenn das Skript keinen Fehler zeigt, dann koennen Sie den Sensor erneut einrichten."
+@echo "https://www.openandhome.de/index.php/ufaqs/waere-es-moeglich-mir-die-daten-fuer-die-grundfiguration-zukommen-zu-lassen/"
+@echo "Druecken Sie eine beliebige Taste . . ." 
+pause
+
