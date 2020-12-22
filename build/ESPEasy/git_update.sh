1a61dea1e34cd2b9e59ee7b2970f40533df78758
Ament Martin <martin.ament@gmx.de>
2020-12-22 22:47:27 +0100

    Adapted new Version of ESP-Easy

--- build/ESPEasy/git_update.sh
+++ build/ESPEasy/git_update.sh
@@ -10,7 +10,7 @@ cp -v $my_pfad/.gitignore myfiles/.gitignore$my_pfad
 cp -v $my_pfad/platformio.ini myfiles/platformio.ini$my_pfad
 cp -v $my_pfad/src/Custom.h myfiles/Custom.h$my_pfad
 cd $my_pfad
-git diff src > ../myfiles/git_diff.patch
+git format-patch origin/mega -o ../myfiles
 #git reset --hard
 #reset hard nicht nötig. Habe mit git checkout -b f-standalone-webserver und git branch -u origin/mega 
 #den Branch so erstellt, dass er meine Änderungen hat und dem Remote-Mega folgt.
