f3dd1818065c78b21c59569551d00ade6022c879
Ament Martin <martin.ament@gmx.de>
2021-04-07 21:36:12 +0200

    Make push_config.sh a little more flexible

--- configs/espeasy/standalone_neu/resources/push_config.sh
+++ configs/espeasy/standalone_neu/resources/push_config.sh
@@ -7,6 +7,7 @@ cd ../resources
 echo "Erstelle aktuelle Version des Dashboards"
 cat header.txt "$old_pwd/table.txt" footer.txt > "$old_pwd/openandhome.esp"
 echo "Sende Icon und Openandhome-Dashboard"
+#http://$1/filelist?delete=openandhome.esp
 curl -v -F Upload=@"$old_pwd/openandhome.esp" http://$1/upload
 curl -v -F Upload=@temperature.png http://$1/upload
 curl -v -F Upload=@temperature1.png http://$1/upload
@@ -15,7 +16,7 @@ curl -v -F Upload=@humidity.png http://$1/upload
 curl -v -F Upload=@openandhome_32.png http://$1/upload
 curl -v -F Upload=@openandhome.css http://$1/upload
 echo "Sende Config"
-curl -v -F Upload=@config.dat http://$1/upload
+#curl -v -F Upload=@config.dat http://$1/upload
 cd "$old_pwd"
 echo 
 echo "Bitte daran denken noch die Adresse des Sensors unter device auszuwählen"
