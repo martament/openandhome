3cec8fa1fb09d81b278b88a65d55a2fa5a8fcd4a
Ament Martin <martin.ament@gmx.de>
2022-12-19 20:53:44 +0100

    deleted old releases

--- build/ESPEasy/build_release.sh
+++ build/ESPEasy/build_release.sh
@@ -3,13 +3,21 @@ set -e
 echo "Setting environment"
 . ../../../configs/env_build.rc
 cd ESPEasy
-pio pkg update
+#pio pkg update
 pio upgrade
 platformio run --target clean
 echo "*********************************************"
-echo "Normal-Build"
+echo "Normal-Build - Standardbox ESP8266"
 echo "*********************************************"
-platformio run --environment normal_ESP8266_4M1M
+platformio run --environment custom_ESP8266_4M1M
+echo
+echo
+#https://docs.platformio.org/en/stable/boards/espressif32/esp32-poe.html
+#platformio run --environment esp32-poe
+echo "*********************************************"
+echo "Custom-Build - Professionalbox ESP32"
+echo "*********************************************"
+platformio run --environment custom_ESP32_4M316k_ETH
 #echo "*********************************************"
 #echo "Devel-Build"
 #echo "*********************************************"
--- build/ESPEasy/cp_release.sh
+++ build/ESPEasy/cp_release.sh
@@ -27,19 +27,48 @@ then
 	#Move files
   	echo "found $buildfile moving it...."
   	cp -av $buildfile ../releases/openandhome_devel.bin | tee >> "$NOTES.new"
- 	mv -v $buildfile ../releases/`date '+%Y%m%d%H%M%S'`_normal_ESP8266_4M1M.bin | tee >> "$NOTES.new"
+ 	mv -v $buildfile ../releases/`date '+%Y%m%d%H%M%S'`_custom_ESP8266_4M1M.bin | tee >> "$NOTES.new"
 	echo >> "$NOTES.new"
 	cat "$NOTES" >> "$NOTES.new"
 	mv "$NOTES.new" "$NOTES"
 	echo "*********************************************************"
 else
 	echo "*********************************************************"
-	echo "No firmware found"
+	echo "No firmware ESP8266 found"
 	echo "*********************************************************"
 fi
+
+#Search for firmware esp32...
+buildfile=`find . -type f -name '*ESP32*bin'| xargs ls | tail -n 1`
+
+if [ -f $buildfile ]
+then
+
+	echo "Creating release from $my_pfad"
+	NOTES="../releases/Release_notes.txt"
+	echo "-------------------------------------------------" > "$NOTES.new"
+	echo "Neues Release32 wurde erzeugt" >> "$NOTES.new"
+	echo "Release32 ist für $my_pfad"
+	echo "-------------------------------------------------" >> "$NOTES.new"
+	echo -e "\nRelease date: `date`\n" >> "$NOTES.new"
+	############## update Release_notes.txt
+	#Move files
+  	echo "found $buildfile moving it...."
+  	cp -av $buildfile ../releases/openandhome_devel32.bin | tee >> "$NOTES.new"
+ 	mv -v $buildfile ../releases/`date '+%Y%m%d%H%M%S'`custom_ESP32_4M316k_ETH.bin | tee >> "$NOTES.new"
+	echo >> "$NOTES.new"
+	cat "$NOTES" >> "$NOTES.new"
+	mv "$NOTES.new" "$NOTES"
+	echo "*********************************************************"
+else
+	echo "*********************************************************"
+	echo "No firmware ESP32 found"
+	echo "*********************************************************"
+fi
+
 
 cd ../releases
 ls -lrtah ./
 echo
-echo "The current Normal-Release can be flashed via flash_espstable"
+echo "The current Custom-Release can be flashed via flash_espstable"
 echo
