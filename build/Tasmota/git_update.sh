91acf09aae8f6a2f113f125997237573011e801f
Ament Martin <martin.ament@gmx.de>
2023-05-12 14:53:11 +0200

    Pfad angepasst

--- build/Tasmota/git_update.sh
+++ build/Tasmota/git_update.sh
@@ -2,13 +2,13 @@
 echo Mache das Git-Update
 echo
 ##echo "Sichere unsere Dateien nach myfiles"
-cp -v Sonoff-Tasmota/tasmota/user_config_override.h myfiles
-cd Sonoff-Tasmota
+cp -v Tasmota/tasmota/user_config_override.h myfiles
+cd Tasmota
 git reset --hard
 git pull
 echo "Wir verwenden den folgenden Branch"
 git config --get remote.origin.url
 git branch
 cd ..
-cp -v myfiles/user_config_override.h Sonoff-Tasmota/tasmota/
+cp -v myfiles/user_config_override.h Tasmota/tasmota/
 #cp -v myfiles/platformio.ini Sonoff-Tasmota/
